4dd44582c899d9aadf24459ebcb9130e6d2b422b
Cedric <cedric.ringger@gmail.com>
2021-02-21 10:33:14 +0100

    Added Mains

---
+++ Client/src/main/java/ch/tbz/kinoverwaltung/client/Main.java
@@ -0,0 +1,7 @@
+package ch.tbz.kinoverwaltung.client;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Server/src/main/java/ch/tbz/kinoverwaltung/server/Main.java
@@ -0,0 +1,7 @@
+package ch.tbz.kinoverwaltung.server;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
